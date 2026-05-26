140ae5b19a28bd4f7213816f4580099eff3affc7
vishalsolo21 <vishalsolo47@gmail.com>
2026-05-26 19:01:39 +0530

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,52 +1,70 @@
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
+  if(req.method !== "POST"){
+
     return res.status(405).json({
-      error: "Method not allowed"
+      success:false,
+      message:"Method Not Allowed"
     });
   }
 
-  try {
+  try{
 
     const { mobile } = req.body;
 
     const response = await fetch(
       "https://www.brevistay.com/cst/app-api/login",
       {
-        method: "POST",
-        headers: {
-          "Accept": "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-          "Authorization": "Bearer null",
-          "brevi-channel": "MOBILE_WEB",
-          "brevi-channel-version": "41.0.0",
-          "User-Agent": "Mozilla/5.0"
+        method:"POST",
+
+        headers:{
+          "Accept":"application/json, text/plain, */*",
+          "Content-Type":"application/json",
+          "Authorization":"Bearer null",
+          "brevi-channel":"MOBILE_WEB",
+          "brevi-channel-version":"41.0.0",
+          "User-Agent":"Mozilla/5.0"
         },
+
         body: JSON.stringify({
-          is_otp: 0,
-          is_password: 1,
-          mobile: mobile,
-          password: "isiwisi",
-          usr_fcm_token: ""
+          is_otp:0,
+          is_password:1,
+          mobile:mobile,
+          password:"isisyy",
+          usr_fcm_token:""
         })
       }
     );
 
     const data = await response.json();
 
+    let registered = false;
+
+    if(
+      data.msg &&
+      data.msg.includes("Wrong Password")
+    ){
+      registered = true;
+    }
+
+    if(
+      data.msg &&
+      data.msg.includes("not a registered user")
+    ){
+      registered = false;
+    }
+
     return res.status(200).json({
-      success: true,
-      registered:
-  data.msg &&
-  data.msg.includes("Wrong Password"),
-      response: data
+      success:true,
+      registered:registered,
+      response:data
     });
 
-  } catch (error) {
+  }catch(error){
 
     return res.status(500).json({
-      success: false,
-      message: error.message
+      success:false,
+      message:error.message
     });
   }
 }
