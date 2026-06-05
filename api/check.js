c4b9a9d8359e06ff69f5bdb8efffa2ae8191c634
vishalsolo21 <vishalsolo47@gmail.com>
2026-06-05 15:02:08 +0530

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,22 +1,5 @@
-export default async function handler(req, res) {
-  try {
-    const { number } = req.query;
-
-    const response = await fetch(
-      `https://ye-lo-mojkro.noob73613.workers.dev/?api_key=@noob11001&number=${number}`
-    );
-
-    const data = await response.json();
-
-    return res.status(200).json(
-      data.first_match || {
-        error: "No data found"
-      }
-    );
-
-  } catch (err) {
-    return res.status(500).json({
-      error: err.message
-    });
-  }
+export default function handler(req, res) {
+  return res.status(200).json({
+    test: "NEW CODE IS RUNNING"
+  });
 }
